ffd03661e8357c25aae8f63b8e62f258a79019c9
John Alalade <johnalalade6@gmail.com>
2021-02-25 12:14:48 -0500

    Updates

---
+++ precache-manifest.32fef5ad34caf9610d9186e22268e4c6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db98b23ea9af93f312cbae021a31a407",
+    "url": "/QuoteBlog/index.html"
+  },
+  {
+    "revision": "2aba3f51b6e932111008",
+    "url": "/QuoteBlog/static/css/2.a6c348fc.chunk.css"
+  },
+  {
+    "revision": "b4d343cb6a50edc3a3bc",
+    "url": "/QuoteBlog/static/css/main.014abd7f.chunk.css"
+  },
+  {
+    "revision": "2aba3f51b6e932111008",
+    "url": "/QuoteBlog/static/js/2.3ca6c6c6.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/QuoteBlog/static/js/2.3ca6c6c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4d343cb6a50edc3a3bc",
+    "url": "/QuoteBlog/static/js/main.92d00a83.chunk.js"
+  },
+  {
+    "revision": "023d7b809cbd7b5757a2",
+    "url": "/QuoteBlog/static/js/runtime-main.dd28aace.js"
+  }
+]);

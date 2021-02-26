b93321212ec8fb93183b7415d4f9e787ebd887db
John Alalade <johnalalade6@gmail.com>
2021-02-25 22:09:21 -0500

    Updates

--- precache-manifest.252e66837aa01e1b3992a2f4336e1192.js
+++ precache-manifest.252e66837aa01e1b3992a2f4336e1192.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "db98b23ea9af93f312cbae021a31a407",
+    "revision": "f3675575295045c6428df8f7eeafb1df",
     "url": "/QuoteBlog/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/QuoteBlog/static/css/2.a6c348fc.chunk.css"
   },
   {
-    "revision": "b4d343cb6a50edc3a3bc",
+    "revision": "ae573056ca0885283092",
     "url": "/QuoteBlog/static/css/main.014abd7f.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/QuoteBlog/static/js/2.3ca6c6c6.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "b4d343cb6a50edc3a3bc",
-    "url": "/QuoteBlog/static/js/main.92d00a83.chunk.js"
+    "revision": "ae573056ca0885283092",
+    "url": "/QuoteBlog/static/js/main.c555182e.chunk.js"
   },
   {
     "revision": "023d7b809cbd7b5757a2",
